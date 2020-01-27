8e582af64c7920349f77e3a5204c8bd4e978d74b
Gabesiee <gabriel.gunullu@edu.esiee.fr>
2020-01-27 16:36:58 +0100

    .

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -5,24 +5,44 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
+import java.io.InputStream;
+
 public class Main extends Application {
+    static Stage stg;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/view/MainView.fxml"));
-        Scene scene = new Scene(root);
-
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("RAPIZZ");
+        this.stg = primaryStage;
         primaryStage.show();
+        primaryStage.setTitle("RAPIZZ");
 
+        InputStream iconStream = getClass().getResourceAsStream("../logo.png");
+        Image image = new Image(iconStream);
+        primaryStage.getIcons().add(image);
+
+        Parent root = FXMLLoader.load(getClass().getResource("/view/mainScene.fxml"));
+        primaryStage.setScene(new Scene(root));
+
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
         Application.launch(args);
     }
 
+    public void menuScene() {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/view/menuScene.fxml"));
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.show();
 
+            Main.stg.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
